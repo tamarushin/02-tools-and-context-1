c4761c131b899f0540a2debd61239648b87ca70c
Tama <tamarushin@gmail.com>
2018-11-11 15:39:14 -0800

    getLength method is working

--- lab-tama/__test__/list.test.js
+++ lab-tama/__test__/list.test.js
@@ -4,20 +4,40 @@ let List = require('../lib/list.js');
 
 describe('list Module', () => {
 
-  it('push() returns the updated length of the list', () => {
+  it('the getLength method returns the length of an array', () => {
+    let myList = new List();
+
+    expect(myList.getLength(['dog', 'cool'])).toBe(2);
+  });
+ 
+  xit('push() returns the updated length of the list', () => {
     let myList = new List();
     myList.push('FOO');
 
     expect(myList.length).toBe(1);
   });
 
-  it('push() adds an element to the end of the list', () => {
+  xit('push() adds an element to the end of the list', () => {
     let myList = new List();
     myList.push('foo');
-    expect(myList[myList.length-1]).toEqual('foo')
+    expect(myList[myList.length-1]).toEqual('foo');
   });
 
-  it('pop() the last element from the list', () => {
+  xit('pop() removes the last element from the list', () => {
+    let myList = new List();
+    myList.pop('FOO');
+    expect(myList.length).toBe(0);
+  });
+
+  describe('testing the map method', () => {
+   xit('should return [2, 4, 6]', () => {
+      let result = List.map ([1, 2, 3], n => n * 2);
+      expect (result).toEqual([2, 4, 6]);
+    });
+    xit('should throw an invalid error', () => {
+
+    });
+
 
   });
 
--- lab-tama/lib/list.js
+++ lab-tama/lib/list.js
@@ -2,25 +2,27 @@
 
 class List {
   constructor () {
-    this.length = 0
+    this.length = 0;
   }
 
-push(item) {
-  this[this.length++] = item;
-  return this.length;
+  push(item) {
+    this[this.length++] = item;
+    return this.length;
+  }
+
+  pop(item) {
+    this[this.length++] = item;
+    return this.length;
+  }
+
+  getLength(array) {
+    let counter = 0;
+    array.forEach(() =>
+      counter++
+    );
+    return counter;
+  }
 }
-
-pop(item) {
-  this[this.length++] = item;
-  return this.length;
-}
-
-
-
-
-}
-
 
 module.exports = List;
-
 
